f6ca21f078cc2d238736f268610e0be84f2fd25e
Danrley Lima <danrleylac@gmail.com>
2022-03-08 01:39:39 -0300

    Final do dia 07/03

--- src/route.js
+++ src/route.js
@@ -6,6 +6,11 @@ route.get('/', (req, res) => res.render("index"))
 route.get('/room', (req, res) => res.render("room"))
 route.get('/create-pass', (req, res) => res.render("create-pass"))
 
+// Formato que o formulário de dentro da modal tem que passar a informação
+// route.get("/room/:room/:question/:action")
+
+
+
 
 module.exports = route
 
