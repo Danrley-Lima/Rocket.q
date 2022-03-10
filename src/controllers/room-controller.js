3e652b5bb11ab6e0fc58e6535e4fd4b3cf057df3
Danrley Lima <danrleylac@gmail.com>
2022-03-10 14:05:59 -0300

    Começar tela sem perguntas

--- src/controllers/room-controller.js
+++ src/controllers/room-controller.js
@@ -7,6 +7,7 @@ module.exports = {
         const pass = req.body.password
         let roomId = ""
         let isRoom = true
+
         while(isRoom) {
             
             for (let i = 0; i < 6; i++) {
@@ -45,9 +46,15 @@ module.exports = {
         const roomId = req.params.room
         const questions = await db.all(`SELECT * FROM questions WHERE room = ${roomId} and read = 0`)
         const questionsRead = await db.all(`SELECT * FROM questions WHERE room = ${roomId} and read = 1`)
-        
-        res.render("room", {roomId: roomId, questions: questions, questionsRead: questionsRead, isNoQuestions: isNoQuestions})
+        let isNoQuestions
 
+        if(questions.length == 0) {
+            if(questionsRead.length == 0) {
+                isNoQuestions = true
+            }
+        }
+        
+        res.render("room", {roomId: roomId, questions: questions, questionsRead: questionsRead})
     },
 
     enter(req, res){
